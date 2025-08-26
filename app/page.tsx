aadb64478ebdd1b1eb05a3c3ffc62c4e6e4ea401
shindeharshad553 <shindeharshad553@gmail.com>
2025-08-26 12:43:11 +0530

    Added edit button

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,8 @@ import { useState, useEffect } from "react";
 const Home = () => {
   const [task, setTask] = useState("");
   const [todos, setTodos] = useState<string[]>([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [EditIndex, setEditIndex] = useState<number | null>(null);
 
   // loads the todos from the localstorage when the page reloads
   useEffect(() => {
@@ -31,6 +33,21 @@ const Home = () => {
     setTodos(newTodos);
   };
 
+  const handleEditClick = (index: number) => {
+    setIsEditing(true);
+    setTask(todos[index]);
+    setEditIndex(index);
+  };
+
+  const updateTodo = () => {
+    if (EditIndex == null) return null;
+    const updatedTodos = [...todos];
+    updatedTodos[EditIndex] = task.trim();
+    setTodos(updatedTodos);
+    setIsEditing(false);
+    setTask("");
+  };
+
   return (
     <main className="flex flex-col items-center p-8">
       <h1 className="text-3xl font-bold mb-4">To do app</h1>
@@ -42,12 +59,21 @@ const Home = () => {
           className="border w-64 rounded p-1"
           onChange={(e) => setTask(e.target.value)}
         />
-        <button
-          className="bg-blue-400 text-white rounded p-2"
-          onClick={AddTodo}
-        >
-          Add todo
-        </button>
+        {isEditing ? (
+          <button
+            className="bg-blue-400 text-white rounded p-2"
+            onClick={updateTodo}
+          >
+            Update todo
+          </button>
+        ) : (
+          <button
+            className="bg-blue-400 text-white rounded p-2"
+            onClick={AddTodo}
+          >
+            Add todo
+          </button>
+        )}
       </div>
 
       {/* display todo list  */}
@@ -58,12 +84,22 @@ const Home = () => {
             className="px-4 py-2 bg-grey rounded  flex justify-between items-center"
           >
             <span>{todo}</span>
-            <button
-              className="bg-red-400 text-white rounded p-2 m-2"
-              onClick={() => deleteTodo(i)}
-            >
-              Delete
-            </button>
+            <div>
+              <button
+                className="bg-red-400 text-white rounded p-2 m-2"
+                onClick={() => deleteTodo(i)}
+              >
+                Delete
+              </button>
+              <button
+                className="bg-blue-400 text-white rounded p-2 m-2"
+                onClick={() => {
+                  handleEditClick(i);
+                }}
+              >
+                Edit
+              </button>
+            </div>
           </li>
         ))}
       </ul>
